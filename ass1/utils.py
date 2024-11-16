69e7d5d0472b6c4d5daaa8fc07b1e2d4c3eac08a
Adam Domoslawski <37457625+RabDou44@users.noreply.github.com>
2024-11-16 17:37:30 +0100

    update of utils + breast cancer analysis

--- ass1/utils.py
+++ ass1/utils.py
@@ -4,12 +4,10 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline, make_pipeline
 import numpy as np 
+import pandas as pd
 import copy
 import time 
 
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
@@ -28,7 +26,7 @@ def get_pipeline(feature_structure, clf = RandomForestClassifier()):
     """
     categorical_preprocessor = Pipeline(
         steps=[
-        ('onehot', OneHotEncoder())
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))
         ])
 
     numerical_preprocessor = Pipeline([
@@ -41,7 +39,7 @@ def get_pipeline(feature_structure, clf = RandomForestClassifier()):
         ('numerical', numerical_preprocessor, feature_structure['cont'])
     ])
 
-    return make_pipeline(preprocessor, clf)
+    return Pipeline(steps=[('preprocessor', preprocessor), ('classifier', clf)])
 
 
 def evaluate_models(data, feature_structure, classifiers):
@@ -74,21 +72,48 @@ def evaluate_models(data, feature_structure, classifiers):
         model_cv = copy.deepcopy( model_holdout)
 
         # Holdout
-        trainX, testX, trainY, testY = train_test_split(X, y, test_size=0.2, random_state=42)
-        start_holdout= time.time()
-        model_holdout.fit( trainX, trainY)
-        end_holdout = time.time()   
-        pred_y  = model_holdout.predict(testX)
-
-        results[str(clf) + "_holdout"] =    [accuracy_score(testY, pred_y),  
-                                             precision_score(testY, pred_y, average="weighted"), 
-                                            recall_score(testY, pred_y,average="weighted"), 
-                                            f1_score(testY, pred_y, average="weighted")]
+        res_holdout, model_holdout = perform_holdout(X, y, model_holdout)
 
         # Cross-validation
-        cv_results = cross_validate(model_cv, X, y, cv=5, scoring=["accuracy", "precision_weighted", "recall_weighted", "f1_weighted"], verbose=1)
-        results[str(clf) + "_cv"] = cv_results
+        res_cv, model_cv = perform_cv(X, y, model_cv)
+
+        results = append_results(results, model_holdout, model_cv, res_holdout, res_cv)
     
+    return pd.DataFrame(results)
+
+def append_results(results, model_holdout, model_cv, res_holdout, res_cv):
+    """
+    Appends the results of the holdout and cross-validation to the results dictionary.
+
+    Parameters
+    -----------
+    results: dict
+        The dictionary containing the results.
+    model_holdout: sklearn pipeline
+        The pipeline used for the holdout evaluation.
+    model_cv: sklearn pipeline
+        The pipeline used for the cross-validation evaluation.
+    res_holdout: dict
+        The results of the holdout evaluation.
+    res_cv: dict
+        The results of the cross-validation evaluation.
+    """
+    model_cv_name = str(model_cv.steps[1][1]) + "_CV"
+    model_holdout_name = str(model_holdout.steps[1][1]) + "_Holdout"
+
+    if results:
+        results["model"] += [model_holdout_name, model_cv_name]
+        for key in res_holdout.keys():
+            results[key] += [res_holdout[key], res_cv[key]]
+    else:
+        results = {
+            "model": [model_holdout_name, model_cv_name],
+            "accuracy": [res_holdout["accuracy"], res_cv["accuracy"]],
+            "precision": [res_holdout["precision"], res_cv["precision"]],
+            "recall": [res_holdout["recall"], res_cv["recall"]],
+            "f1-score": [res_holdout["f1-score"], res_cv["f1-score"]],
+            "timing": [res_holdout["timing"], res_cv["timing"]]
+        }
     return results
 
 
@@ -106,7 +131,7 @@ def perform_holdout(X, y, clf, random_state=42):
     clf: sklearn classifier
         The classifier to use in the pipeline.
     random_state: int
-        The random state to use for the train-test split.
+        The random state to suse for the train-test split.
 
     
     Keyword arguments:
@@ -118,17 +143,18 @@ def perform_holdout(X, y, clf, random_state=42):
     trainX, testX, trainY, testY = train_test_split(X, y, test_size=0.2, random_state=random_state)
     start_time = time.time()
     model_holdout.fit( trainX, trainY)
-    end_time = time.time()
     pred_y  = model_holdout.predict(testX)
+    fitting_time = time.time() - start_time
 
-    results = [accuracy_score(testY, pred_y),  
-             precision_score(testY, pred_y, average="weighted"), 
-            recall_score(testY, pred_y,average="weighted"), 
-            f1_score(testY, pred_y, average="weighted"), end_time - start_time] 
+    results = {"accuracy":accuracy_score(testY, pred_y),  
+             "precision": precision_score(testY, pred_y, average="weighted"), 
+            "recall":recall_score(testY, pred_y,average="weighted"), 
+            "f1-score": f1_score(testY, pred_y, average="weighted"), 
+            "timing": fitting_time}
 
-    return (results, model_holdout)
+    return results, model_holdout
 
-def perform_cv(X, y, clf):
+def perform_cv(X, y, clf): 
     """
     Builds a model using the pipeline and returns it
     with split data and specified parameters 
@@ -148,6 +174,14 @@ def perform_cv(X, y, clf):
     """
 
     model_cv = clf
-    cv_results = cross_validate(model_cv, X, y, cv=5, scoring=["accuracy", "precision_weighted", "recall_weighted", "f1_weighted"], verbose=1)
-    
-    return cv_results
+    start_time = time.time()
+    cv_results = cross_validate(model_cv, X, y, cv=5, scoring=["accuracy", "precision_weighted", "recall_weighted", "f1_weighted"], verbose=1, return_estimator=True)
+    whole_time = time.time() - start_time
+    res = {"accuracy": np.nanmean(cv_results["test_accuracy"])
+           , "precision": np.nanmean(cv_results["test_precision_weighted"]),
+           "recall": np.nanmean(cv_results["test_recall_weighted"]),
+           "f1-score": np.nanmean(cv_results["test_f1_weighted"]),
+              "timing": whole_time}
+    best_estimator = cv_results["estimator"][np.argmax(cv_results["test_accuracy"])]
+
+    return (res, best_estimator) 
