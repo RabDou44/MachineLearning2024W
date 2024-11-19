a768cd6fc07a74e4a33380d33fa8410cf46a9477
Adam Domoslawski <37457625+RabDou44@users.noreply.github.com>
2024-11-19 20:33:31 +0100

    porto script

--- ass1/run_porto.py
+++ ass1/run_porto.py
@@ -7,6 +7,7 @@ import pickle
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
+import pickle
 
 
 
@@ -23,15 +24,13 @@ def main():
     X = data[feature_columns]
     y = data['target']
 
-
-
     # # classifierKneighbors = [ KNeighborsClassifier(n_jobs=-1, n_neighbors=k) for k in range(2, 13)]
-    classifierKneighbors = [ KNeighborsClassifier(n_jobs=30, n_neighbors=1)]
-    resultsKNeighbors = evaluate_models(data, feature_structure, classifierKneighbors)
-    print(results_to_latex(resultsKNeighbors, "Results Porto k-NN", "porto_knn"))
-    # classifierDecisionTree = [ DecisionTreeClassifier(random_state=42, max_depth=depth) for depth in range(3, 15)]
-    # resultsDecisionTree = evaluate_models(data, feature_structure, classifierDecisionTree)
-    # print(results_to_latex(resultsDecisionTree, "Results Porto Decision Trees", "porto_dt"))
+    classifierKneighbors = [ SVC(kernel='linear')]
+    results, model = perform_holdout(X,y, classifierKneighbors[0])
+    
+    # save + print
+    print(results) 
+    pickle.dump(model, open("./porto-knn-model.pkl", 'wb'))
 
 
 if __name__ == "__main__":
